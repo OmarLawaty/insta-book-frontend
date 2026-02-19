eb0a7d0bd53259520c2dc03d51b8379f291cb946
OmarLawaty <omarlawatey@gmail.com>
2026-02-19 02:40:51 +0200

    fix: Update API endpoint in useTopUsersQuery to fetch top 8 users

--- src/hooks/users/useTopUsersQuery.ts
+++ src/hooks/users/useTopUsersQuery.ts
@@ -5,7 +5,7 @@ import { instabook, TopUser } from '@/api';
 
 const queryKey = ['top-users'] as const;
 
-const queryFn = () => instabook.get<TopUser[], AxiosResponse<TopUser[]>>('/users/top/10').then(res => res.data);
+const queryFn = () => instabook.get<TopUser[], AxiosResponse<TopUser[]>>('/users/top/8').then(res => res.data);
 
 export const useTopUsersQuery = () => useQuery({ queryKey, queryFn });
 
